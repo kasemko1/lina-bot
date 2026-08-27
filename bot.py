f30c5bea340b745b469b456956183b31a20ced86
kasemko1 <kassemmoai19@gmail.com>
2026-08-27 04:01:41 +0200

    Fix return statement indentation in bot.py

--- bot.py
+++ bot.py
@@ -125,7 +125,7 @@ if call.data == "web3":
             f"بعد الدفع، يرجى إرسال رقم العملية (Tx Hash) الذي يبدأ بـ (0x...).",
             parse_mode="Markdown",
         )
-       return                
+            return                
     user_data[user_id] = {"type": call.data, "step": 1}
     price = "2.99€ اشتراك شهري" if call.data == "sub" else "0.50€ رسوم فتح الطلب"
     keyboard = InlineKeyboardMarkup().add(
